5b5e8baf893b9fb0501af506cf19d1b836c0c2e3
dev_khanhnp <nguyenphiikhanh@gmail.com>
2022-09-15 23:50:11 +0700

    #auth conf: backend sanctum auth config

---
+++ backend/app/Http/Utils/AppUtils.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Create info
+ * User: khanhnp
+ * Time: 2022/09/15 11:37pm
+ */
+
+namespace App\Http\Utils;
+
+class AppUtils{
+    // user permissions
+    const USER_ROOT = 0;
+    const USER_CBQL = 1;
+    const USER_CVHT = 2;
+    const USER_CBL = 3;
+    const USER_NOMAL = 4;
+}
