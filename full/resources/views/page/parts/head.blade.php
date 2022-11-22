3cc4c14072d9eb1aa6e04998309d51b349db6920
dev_khanhnp <nguyenphiikhanh@gmail.com>
2022-11-22 22:36:11 +0700

    remove unnessecary files

--- full/resources/views/page/parts/head.blade.php
+++ full/resources/views/page/parts/head.blade.php
@@ -67,12 +67,12 @@
                         <div class="user-nav d-sm-flex d-none"><span
                                 class="user-name fw-bolder">{{ $user->last_name }}</span><span
                                 class="user-status">Admin</span></div><span class="avatar"><img class="round"
-                                src="{{ asset('app-assets/images/portrait/small/avatar-s-11.jpg') }}" alt="avatar"
+                                src="{{ asset('app-assets/images/avatars/423426-200.png') }}" alt="avatar"
                                 height="40" width="40"><span class="avatar-status-online"></span></span>
                     </a>
                     <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-user">
-                        <a class="dropdown-item" href="page-profile.html"><i class="me-50" data-feather="user"></i>
-                            Trang cá nhân</a>
+                        <a class="dropdown-item" href="page-profile.html"><i class="me-50" data-feather="key"></i>
+                             Đổi mật khẩu</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="{{ route('logout') }}"><i class="me-50"
                                 data-feather="power"></i> Đăng xuất</a>
@@ -81,110 +81,4 @@
             </ul>
         </div>
     </nav>
-    <ul class="main-search-list-defaultlist d-none">
-        <li class="d-flex align-items-center"><a href="#">
-                <h6 class="section-label mt-75 mb-0">Files</h6>
-            </a></li>
-        <li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between w-100"
-                href="app-file-manager.html">
-                <div class="d-flex">
-                    <div class="me-75"><img src="{{ asset('app-assets/images/icons/xls.png') }}" alt="png"
-                            height="32"></div>
-                    <div class="search-data">
-                        <p class="search-data-title mb-0">Two new item submitted</p><small
-                            class="text-muted">Marketing Manager</small>
-                    </div>
-                </div><small class="search-data-size me-50 text-muted">&apos;17kb</small>
-            </a></li>
-        <li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between w-100"
-                href="app-file-manager.html">
-                <div class="d-flex">
-                    <div class="me-75"><img src="{{ asset('app-assets/images/icons/jpg.png') }}" alt="png"
-                            height="32"></div>
-                    <div class="search-data">
-                        <p class="search-data-title mb-0">52 JPG file Generated</p><small class="text-muted">FontEnd
-                            Developer</small>
-                    </div>
-                </div><small class="search-data-size me-50 text-muted">&apos;11kb</small>
-            </a></li>
-        <li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between w-100"
-                href="app-file-manager.html">
-                <div class="d-flex">
-                    <div class="me-75"><img src="{{ asset('app-assets/images/icons/pdf.png') }}" alt="png"
-                            height="32"></div>
-                    <div class="search-data">
-                        <p class="search-data-title mb-0">25 PDF File Uploaded</p><small class="text-muted">Digital
-                            Marketing Manager</small>
-                    </div>
-                </div><small class="search-data-size me-50 text-muted">&apos;150kb</small>
-            </a></li>
-        <li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between w-100"
-                href="app-file-manager.html">
-                <div class="d-flex">
-                    <div class="me-75"><img src="{{ asset('app-assets/images/icons/doc.png') }}" alt="png"
-                            height="32"></div>
-                    <div class="search-data">
-                        <p class="search-data-title mb-0">Anna_Strong.doc</p><small class="text-muted">Web
-                            Designer</small>
-                    </div>
-                </div><small class="search-data-size me-50 text-muted">&apos;256kb</small>
-            </a></li>
-        <li class="d-flex align-items-center"><a href="#">
-                <h6 class="section-label mt-75 mb-0">Members</h6>
-            </a></li>
-        <li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between py-50 w-100"
-                href="app-user-view-account.html">
-                <div class="d-flex align-items-center">
-                    <div class="avatar me-75"><img
-                            src="{{ asset('app-assets/images/portrait/small/avatar-s-8.jpg') }}" alt="png"
-                            height="32"></div>
-                    <div class="search-data">
-                        <p class="search-data-title mb-0">John Doe</p><small class="text-muted">UI designer</small>
-                    </div>
-                </div>
-            </a></li>
-        <li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between py-50 w-100"
-                href="app-user-view-account.html">
-                <div class="d-flex align-items-center">
-                    <div class="avatar me-75"><img
-                            src="{{ asset('app-assets/images/portrait/small/avatar-s-1.jpg') }}" alt="png"
-                            height="32"></div>
-                    <div class="search-data">
-                        <p class="search-data-title mb-0">Michal Clark</p><small class="text-muted">FontEnd
-                            Developer</small>
-                    </div>
-                </div>
-            </a></li>
-        <li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between py-50 w-100"
-                href="app-user-view-account.html">
-                <div class="d-flex align-items-center">
-                    <div class="avatar me-75"><img
-                            src="{{ asset('app-assets/images/portrait/small/avatar-s-14.jpg') }}" alt="png"
-                            height="32"></div>
-                    <div class="search-data">
-                        <p class="search-data-title mb-0">Milena Gibson</p><small class="text-muted">Digital Marketing
-                            Manager</small>
-                    </div>
-                </div>
-            </a></li>
-        <li class="auto-suggestion"><a class="d-flex align-items-center justify-content-between py-50 w-100"
-                href="app-user-view-account.html">
-                <div class="d-flex align-items-center">
-                    <div class="avatar me-75"><img
-                            src="{{ asset('app-assets/images/portrait/small/avatar-s-6.jpg') }}" alt="png"
-                            height="32"></div>
-                    <div class="search-data">
-                        <p class="search-data-title mb-0">Anna Strong</p><small class="text-muted">Web
-                            Designer</small>
-                    </div>
-                </div>
-            </a></li>
-    </ul>
-    <ul class="main-search-list-defaultlist-other-list d-none">
-        <li class="auto-suggestion justify-content-between"><a
-                class="d-flex align-items-center justify-content-between w-100 py-50">
-                <div class="d-flex justify-content-start"><span class="me-75"
-                        data-feather="alert-circle"></span><span>No results found.</span></div>
-            </a></li>
-    </ul>
     <!-- END: Header-->
