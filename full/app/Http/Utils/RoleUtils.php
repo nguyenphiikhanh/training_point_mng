327c743d0885d46edecda3c8958f336593321890
dev_khanhnp <nguyenphiikhanh@gmail.com>
2022-11-22 21:02:28 +0700

    update user seeder

---
+++ full/app/Http/Utils/RoleUtils.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Create info
+ * User: khanhnp
+ * Time: 2022/09/15 11:37pm
+ */
+
+namespace App\Http\Utils;
+
+class RoleUltils{
+    // user permissions
+    const ROLE_ROOT = 0;
+    const ROLE_CBQL = 1;
+    const ROLE_CVHT = 2;
+    const ROLE_CBL = 3;
+    const ROLE_STUDENT = 4;
+
+    public static function getRoleName($role_flg){
+        switch($role_flg){
+            // case self::
+        }
+    }
+}
