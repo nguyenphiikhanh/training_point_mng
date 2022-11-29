7bf61fc97b6a8668a6e791569085340becca71e1
dev_khanhnp <nguyenphiikhanh@gmail.com>
2022-11-30 01:05:35 +0700

    #khanhnp feat: change password

--- full/routes/web.php
+++ full/routes/web.php
@@ -27,11 +27,14 @@ Route::get('logout', [LoginController::class,'logout'])->name('logout');
 Route::middleware('user.authenticated')->prefix('')->name('page.')->group(function () {
     Route::get('/',[DashBoardController::class,'index'])->name('index');
 
+    Route::get('/doi-mat-khau',[UserController::class,'password_change_view'])->name('password.change.view');
+    Route::post('/doi-mat-khau',[UserController::class,'password_change'])->name('password.change.store');
+
     //BEGIN: quản lí khoa. Role: ADMIN
     Route::get('/quan-li-khoa',[FacultController::class,'index'])->name('faculty.list');
     Route::post('/quan-li-khoa',[FacultController::class,'store'])->name('faculty.store');
     Route::post('/quan-li-khoa/{id}/update',[FacultController::class,'update'])->name('faculty.update');
-    Route::get('/quan-li-khoa/{id}/delete',[FacultController::class,'destroy'])->name('faculty.destroy');
+    Route::put('/quan-li-khoa/{id}/delete',[FacultController::class,'destroy'])->name('faculty.destroy');
     //END: quản lí khoa.
 
     //BEGIN: quản lí đợt duyệt. Role: ADMIN
