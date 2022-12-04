b7e9ceed853f09d7750424f4daeef16dd8d09a15
dev_khanhnp <nguyenphiikhanh@gmail.com>
2022-12-04 20:38:36 +0700

    #khanhnp feat: open confirm times(store)

--- full/app/Http/Controllers/TimeController.php
+++ full/app/Http/Controllers/TimeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Utils\AppUtils;
+use App\Models\DotXetDuyet;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TimeController extends Controller
 {
@@ -11,10 +14,15 @@ class TimeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         //
-        return view('page.admin.times.list');
+        $times = DotXetDuyet::orderBy('id','desc');
+        if($request->details){
+            $times->where('name','like',"%$request->details%");
+        }
+        $times = $times->paginate(AppUtils::ITEMS_PER_PAGE);
+        return view('page.admin.times.list',['times' => $times]);
     }
 
     /**
@@ -26,6 +34,23 @@ class TimeController extends Controller
     public function store(Request $request)
     {
         //
+        try{
+            $time_content = $request->time_content;
+            $end_time = $request->end_time;
+            //convert end_time
+            $end_time = str_replace('/','-',$end_time);
+            //
+            DotXetDuyet::create([
+                'name' => $time_content,
+                'deadline' => date('Y-m-d H:i',strtotime($end_time)),
+            ]);
+
+            return back()->with('success','Mở xét duyệt thành công!');
+        }
+        catch(\Exception $e){
+            Log::error($e->getMessage(). $e->getTraceAsString());
+            return back()->with('error',__('custom_message.failed'));
+        }
     }
 
     /**
