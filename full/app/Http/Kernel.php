5d27475781c79d73263c04a3d8257788635f4414
dev_khanhnp <nguyenphiikhanh@gmail.com>
2022-12-22 21:25:10 +0700

    admin permission && error pages

--- full/app/Http/Kernel.php
+++ full/app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'user.authenticated' => \App\Http\Middleware\UserAuthenticated::class,
+        'role.admin' => \App\Http\Middleware\CheckAdminRole::class,
     ];
 }
